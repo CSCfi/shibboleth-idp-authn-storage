787a7ac47a7368c376fbed45d19ccd16055a436e
jalauros <janne.lauros@gmail.com>
2018-04-11 13:27:17 +0300

    key length reduces to 32 chars

--- idp-authn-api-storage/src/main/java/fi/csc/idp/authn/storage/AuthenticationEventCache.java
+++ idp-authn-api-storage/src/main/java/fi/csc/idp/authn/storage/AuthenticationEventCache.java
@@ -147,8 +147,7 @@ public class AuthenticationEventCache extends AbstractIdentifiableInitializableC
     @SuppressWarnings("rawtypes")
     public AuthenticationEvent locate(@Nonnull @NotEmpty final String userKey) {
         String key;
-        // TODO: replace with salted digest
-        key = DigestUtils.sha256Hex(userKey + userSalt);
+        key = DigestUtils.sha256Hex(userKey + userSalt).substring(0,32);
         log.debug("User {} hashed to {}", userKey, key);
         lock.lock();
         try {
@@ -174,8 +173,7 @@ public class AuthenticationEventCache extends AbstractIdentifiableInitializableC
 
     public boolean set(@Nonnull @NotEmpty final String userKey, @Nonnull @NotEmpty final AuthenticationEvent value) {
         String key;
-        // TODO: replace with salted digest
-        key = DigestUtils.sha256Hex(userKey + userSalt);
+        key = DigestUtils.sha256Hex(userKey + userSalt).substring(0,32);
         log.debug("User {} hashed to {}", userKey, key);
         lock.lock();
         try {
